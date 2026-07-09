22e5c80c0409239c50271ec58a7d9656c538865e
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-07-09 12:18:54 +0330

    Main Update!

--- Main.py
+++ Main.py
@@ -27,3 +27,15 @@ def split(A):
     A21 = [[A[i][j] for j in range(m)] for i in range(m,n)]
     A22 = [[A[i][j] for j in range(m,n)] for i in range(m,n)]
     return A11,A12,A21,A22
+
+def merge(C11,C12,C21,C22):
+    m = len(C11)
+    C = [[0]*(m*2) for _ in range(m*2)]
+    for i in range(m):
+        for j in range(m):
+            C[i][j] = C11[i][j]
+            C[i][j+m] = C12[i][j]
+            C[i+m][j] = C21[i][j]
+            C[i+m][j+m] = C22[i][j]
+    return C
+
