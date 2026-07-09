8720d2f6173b2d18905df9219358972404fa48cb
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-07-09 12:18:27 +0330

    Main Update!

--- Main.py
+++ Main.py
@@ -18,3 +18,12 @@ def add(A, B):
 def sub(A, B):
     n = len(A)
     return [[A[i][j]-B[i][j] for j in range(n)] for i in range(n)]
+
+def split(A):
+    n = len(A)
+    m = n//2
+    A11 = [[A[i][j] for j in range(m)] for i in range(m)]
+    A12 = [[A[i][j] for j in range(m,n)] for i in range(m)]
+    A21 = [[A[i][j] for j in range(m)] for i in range(m,n)]
+    A22 = [[A[i][j] for j in range(m,n)] for i in range(m,n)]
+    return A11,A12,A21,A22
